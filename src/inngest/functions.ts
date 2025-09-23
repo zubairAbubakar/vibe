f76241f07a4b1802b5986a52af6043609dbdbe59
Zubair Abubakar <zuby52@gmail.com>
2025-09-23 08:10:51 +0100

    [VIBE-08] Setup coding agent with some tools

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,9 +1,9 @@
 import { Sandbox } from '@e2b/code-interpreter';
-import { openai, gemini, createAgent } from '@inngest/agent-kit';
+import { gemini, createAgent, createTool } from '@inngest/agent-kit';
 
 import { inngest } from './client';
-import { get } from 'http';
 import { getSandbox } from './utils';
+import { tr } from 'date-fns/locale';
 
 export const helloWorld = inngest.createFunction(
   { id: 'hello-world' },
@@ -20,6 +20,67 @@ export const helloWorld = inngest.createFunction(
       system:
         'You are an expert nextjs developer. You write readable, maintainable code. You write simple Next.js & React code snippets.',
       model: gemini({ model: 'gemini-2.0-flash' }),
+      tools: [
+        createTool({
+          name: 'terminal',
+          description:
+            'Use the terminal to run commands in the code interpreter sandbox.',
+          handler: async ({ command }: { command: string }, { step }) => {
+            return await step?.run('terminal', async () => {
+              const buffer = { stdout: '', stderr: '' };
+              try {
+                const sandbox = await getSandbox(sandboxId);
+                const result = await sandbox.commands.run(command, {
+                  onStdout: (data: string) => {
+                    buffer.stdout += data;
+                  },
+                  onStderr: (data: string) => {
+                    buffer.stderr += data;
+                  },
+                });
+                return result.stdout;
+              } catch (error) {
+                console.error(
+                  `Command failed: ${error} \nstdout: ${buffer.stdout} \nstderr: ${buffer.stderr}`
+                );
+                return `Command failed: ${error} \nstdout: ${buffer.stdout} \nstderr: ${buffer.stderr}`;
+              }
+            });
+          },
+        }),
+        createTool({
+          name: 'createOrUpdateFiles',
+          description:
+            'Create or update files in the code interpreter sandbox.',
+          handler: async (
+            { files }: { files: Array<{ path: string; content: string }> },
+            { step, network }
+          ) => {
+            const newFiles = await step?.run(
+              'createOrUpdateFiles',
+              async () => {
+                try {
+                  const updatedFiles = network.state.data.files || {};
+                  const sandbox = await getSandbox(sandboxId);
+                  for (const file of files) {
+                    await sandbox.files.write(file.path, file.content);
+                    updatedFiles[file.path] = file.content;
+                  }
+                  return updatedFiles;
+                } catch (error) {
+                  return {
+                    error: `Failed to create or update files: ${error}`,
+                  };
+                }
+              }
+            );
+
+            if (typeof newFiles === 'object') {
+              network.state.data.files = newFiles;
+            }
+          },
+        }),
+      ],
     });
 
     const { output } = await codeAgent.run(
