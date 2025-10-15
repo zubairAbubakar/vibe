88d770d51d1a21ef7fdd72f3a452c6c030ec4a10
Zubair Abubakar <zuby52@gmail.com>
2025-10-15 22:59:06 +0100

    [VIBE-08] code view setup

--- src/modules/projects/ui/views/project-view.tsx
+++ src/modules/projects/ui/views/project-view.tsx
@@ -1,17 +1,23 @@
 'use client';
 
+import Link from 'next/link';
 import { Suspense, useState } from 'react';
+import { useSuspenseQuery } from '@tanstack/react-query';
+import { CodeIcon, CrownIcon, EyeIcon } from 'lucide-react';
+
 import {
   ResizableHandle,
   ResizablePanel,
   ResizablePanelGroup,
 } from '@/components/ui/resizable';
 import { useTRPC } from '@/trpc/client';
-import { useSuspenseQuery } from '@tanstack/react-query';
 import { ProjectHeader } from '../components/project-header';
 import { MessagesContainer } from '../components/messages-container';
 import { Fragment } from '@/generated/prisma';
 import { FragmentWebview } from '../components/fragment-webview';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import { Button } from '@/components/ui/button';
+import { CodeView } from '@/components/code-view';
 
 interface Props {
   projectId: string;
@@ -19,6 +25,7 @@ interface Props {
 
 export const ProjectView = ({ projectId }: Props) => {
   const [activeFragment, setActiveFragment] = useState<Fragment | null>(null);
+  const [tabState, setTabState] = useState<'preview' | 'code'>('preview');
 
   const trpc = useTRPC();
 
@@ -48,7 +55,36 @@ export const ProjectView = ({ projectId }: Props) => {
         </ResizablePanel>
         <ResizableHandle withHandle />
         <ResizablePanel defaultSize={65} minSize={50}>
-          {!!activeFragment && <FragmentWebview data={activeFragment} />}
+          <Tabs
+            className="h-full gap-y-0"
+            defaultValue="preview"
+            value={tabState}
+            onValueChange={(value) => setTabState(value as 'preview' | 'code')}
+          >
+            <div className="w-full flex items-center p-2 border-b gap-x-2">
+              <TabsList className="h-8 p-0 border rounded-md">
+                <TabsTrigger value="preview" className="rounded-md">
+                  <EyeIcon /> <span>Demo</span>
+                </TabsTrigger>
+                <TabsTrigger value="code" className="rounded-md">
+                  <CodeIcon /> <span>Code</span>
+                </TabsTrigger>
+              </TabsList>
+              <div className="ml-auto flex items-center gap-x-2">
+                <Button asChild size="sm" variant="default">
+                  <Link href="/pricing">
+                    <CrownIcon /> Upgrade
+                  </Link>
+                </Button>
+              </div>
+            </div>
+            <TabsContent value="preview">
+              {!!activeFragment && <FragmentWebview data={activeFragment} />}
+            </TabsContent>
+            <TabsContent value="code">
+              <CodeView language="ts" code="console.log('Hello, world!');" />
+            </TabsContent>
+          </Tabs>
         </ResizablePanel>
       </ResizablePanelGroup>
     </div>
